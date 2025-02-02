cfd94cc6aea72d6ecfa617dcb198087a3f8ab1d4
serhatgenc74 <serhatgenc74@gmail.com>
2025-02-02 15:31:21 +0300

    second commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectDunyam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
